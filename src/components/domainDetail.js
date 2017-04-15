9714b47cb4bf01ef769d11539408fa9a1ab558af
Tsinat Zeree <cheru87@gmail.com>
2017-04-15 11:15:06 -0700

    beautifying domain detail.js

--- src/components/domainDetail.js
+++ src/components/domainDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router';
+import {Link} from 'react-router';
 import PropTypes from 'prop-types';
 
 class DomainDetail extends React.Component {
@@ -15,7 +15,9 @@ class DomainDetail extends React.Component {
     }
 
     handleChange(e) {
-        this.setState({[e.target.name]: e.target.value})
+        this.setState({
+            [e.target.name]: e.target.value
+        })
     }
 
     handleSave(e) {
@@ -32,35 +34,22 @@ class DomainDetail extends React.Component {
                         <label className="">Domain Name</label>
                     </div>
                     <div className="col-xs-9">
-                        <input type="text"
-                            name="domain"
-                            value={this.state.domain}
-                            onChange={this.handleChange}
-                            className="form-control col-xs-6" id="name"/>
+                        <input type="text" name="domain" value={this.state.domain} onChange={this.handleChange} className="form-control col-xs-6" id="name"/>
                     </div>
                     <div className="col-xs-3">
                         <label className="">Registrant Email</label>
                     </div>
                     <div className="col-xs-9">
-                        <input type="text"
-                            name="email"
-                            value={this.state.email}
-                            onChange={this.handleChange}
-                            className="form-control"  id="email"/>
+                        <input type="text" name="email" value={this.state.email} onChange={this.handleChange} className="form-control" id="email"/>
                     </div>
                     <div className="col-xs-3">
                         <label className="">Price</label>
                     </div>
                     <div className="col-xs-9">
-                        <input type="text"
-                            name="price"
-                            value={`${price}`}
-                            onChange={this.handleChange}
-                            className="form-control" id="price" />
+                        <input type="text" name="price" value={`${price}`} onChange={this.handleChange} className="form-control" id="price"/>
                     </div>
                     <div className="col-xs-9 col-xs-offset-3">
-                        <button type="submit" className="btn"
-                            onClick={ this.handleSave }>
+                        <button type="submit" className="btn" onClick={this.handleSave}>
                             Save changes
                         </button>
                     </div>
@@ -71,7 +60,7 @@ class DomainDetail extends React.Component {
 }
 
 DomainDetail.contextTypes = {
-  router: PropTypes.object.isRequired
+    router: PropTypes.object.isRequired
 }
 
 export default DomainDetail;
